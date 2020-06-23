30bf3d4e3653353ea14029130ae2cd17326e5a93
Arthur <46713689+ruz1k@users.noreply.github.com>
2020-06-23 11:34:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,43 @@
 import telebot
 import mysql.connector
+from telebot import types
 
-db = mysql.connector.connect(
-      host="localhost",
-      user="root",
-      passwd="root",
-      port="3306",
-      database="telebot",
-)
-
-cursor = db.cursor()
-
-# cursor.execute("SELECT * FROM customers")
-# cursor.execute("CREATE TABLE telepy (id INT AUTO_INCREMENT PRIMARY KEY, date_otpravki DATETIME, date_otveta DATETIME)")
-# myresult = cursor.fetchall()
-# #cursor.execute("DROP TABLE user_date")
-# cursor.execute("INSERT INTO telepy SET date_otpravki = NOW(), date_otveta = NOW()")
-# db.commit()
-# print(myresult)
-
-
-bot = telebot.TeleBot('1203996678:AAGgShbFSxIsJTIG5OKpuOLRXEL2JTR73_8')
+bot = telebot.TeleBot('***')
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    user = message.from_user.first_name
-    bot.send_message(message.chat.id, "Hello {}".format(user))
-
-@bot.message_handler(commands=['table'])
-def table_command(message):
-    msg = bot.send_message(message.chat.id, "How many days do you want to see the information?")
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_one = types.KeyboardButton(text="1")
+    button_two = types.KeyboardButton(text="2")
+    button_three = types.KeyboardButton(text="3")
+    button_seven = types.KeyboardButton(text="7")
+    keyboard.add(button_one, button_two, button_three, button_seven)
+    msg = bot.send_message(message.chat.id, "How many days do you want to see the information?", reply_markup=keyboard)
     bot.register_next_step_handler(msg, table_week)
 
+@bot.message_handler(func=lambda message:True)
 def table_week(message):
     try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="bet2u_db",
+            password="2V6e5R8g",
+            port="3306",
+            database="test"
+            )
+        cursor = mydb.cursor()
         val = message.text
         sql = "SELECT \
-        DATE_FORMAT(`date_otpravki`, '%d %M %H:%i') AS 'date', \
-        IF(TIMEDIFF(`date_otveta`, `date_otpravki`) >= 0, TIMEDIFF(`date_otveta`, `date_otpravki`), 'Нет ответа') AS 'diff'\
-        FROM `telepy` \
-        WHERE `date_otpravki` > DATE_SUB(NOW(), INTERVAL %s DAY)"
+        DATE_FORMAT(`request`, '%d %M %H:%i') AS 'date', \
+        IF( ( TIMEDIFF(`response`, `request`) >= 0 && TIMEDIFF(`response`, `request`) < '00:30:00'), TIMEDIFF(`response`, `request`), 'Нет ответа') AS 'diff'\
+        FROM `maksBot` \
+        WHERE `request` > DATE_SUB(NOW(), INTERVAL %s DAY)"
         cursor.execute(sql, (val, ))
         result = cursor.fetchall()
         for x in result:
-            bot.send_message(message.chat.id, "{}".format(x))
+                bot.send_message(message.chat.id, "{}".format(x))
+        mydb.close()
+        return start_command(message)
     except Exception as e:
         bot.reply_to(message, "Error! \nEnter the number! {}".format(e))
 
